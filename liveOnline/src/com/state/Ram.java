726498ec8a492b4885e438a10594e3eab5037e1d
tany <jayi1314@163.com>
2018-03-22 15:56:58 +0800

    初始化

---
+++ liveOnline/src/com/state/Ram.java
@@ -0,0 +1,16 @@
+package com.state;  
+/** 
+ * 临时容器 
+ * @author Allen 2017年3月31日 
+ * 
+ */  
+  
+import java.util.HashMap;  
+  
+import com.state.room.RoomVo;  
+  
+public class Ram {  
+  
+    public static HashMap<String, RoomVo> roomHm = new HashMap<>();  
+      
+} 
